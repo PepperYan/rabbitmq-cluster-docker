30067d771b3ddab7aaf5fa77c26d9b7b420b1d7d
hibad <coldwinner1@163.com>
2016-06-14 16:29:42 +0800

    no message

--- server/startrabbit.sh
+++ server/startrabbit.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+#
+echo "hosting"
+touch /etc/hosts
+grep "rabbit1" /etc/hosts
+if [ $? -ne 0 ]; then
+  echo "hosts not exist"
+  chmod 777 /etc/hosts
+  echo "139.59.240.247 rabbit1" >> /etc/hosts
+  echo "139.59.240.230 rabbit2" >> /etc/hosts
+  echo "188.166.183.42 rabbit3" >> /etc/hosts
+  sync
+else
+  echo "hosts exist"
+fi
 
 if [ -z "$CLUSTERED" ]; then
 	# if not clustered then start it normally as if it is a single server
-	/usr/sbin/rabbitmq-server	
+	/usr/sbin/rabbitmq-server
 else
 	if [ -z "$CLUSTER_WITH" ]; then
 		# If clustered, but cluster with is not specified then again start normally, could be the first server in the
@@ -22,4 +36,3 @@ else
 		tail -f /var/log/rabbitmq/rabbit\@$HOSTNAME.log
 	fi
 fi
-
